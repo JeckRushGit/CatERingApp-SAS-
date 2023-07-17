03a9e7425c9ef5c55cc903a58cea5eae83a8f1ba
Mehdi Amlal <mehdi.amlal.business@gmail.com>
2023-07-17 17:17:07 +0200

    aggiunto test 5e

---
+++ src/KitchenTaskTest5e.java
@@ -0,0 +1,36 @@
+import businesslogic.CatERing;
+import businesslogic.UseCaseLogicException;
+import businesslogic.event.Event;
+import businesslogic.event.Service;
+import businesslogic.kitchentask.KitchenTask;
+import businesslogic.kitchentask.SummarySheet;
+
+import java.util.List;
+
+public class KitchenTaskTest5e {
+    public static void main(String[] args) {
+        try {
+            System.out.println("TEST FAKE LOGIN");
+            CatERing.getInstance().getUserManager().fakeLogin("Lidia");
+            System.out.println(CatERing.getInstance().getUserManager().getCurrentUser());
+
+            List<Event> list = CatERing.getInstance().getEventManager().getEventInfo();
+
+
+            Event targetEvent = list.get(1);
+            Service service = targetEvent.getServices().get(0);
+
+            System.out.println("TEST OPEN SUMMARY SHEET");
+            SummarySheet sheet = CatERing.getInstance().getKitchenTaskManager().openSummarySheet(targetEvent, service);
+
+            System.out.println("CHANGE TASK TIME ESTIMATE");
+            KitchenTask targetTask = sheet.getTasks().get(0);
+            CatERing.getInstance().getKitchenTaskManager().changeTimeEstimate(targetTask, "25 minuti");
+
+
+        } catch (UseCaseLogicException e) {
+            System.out.println("Errore di logica nello use case");
+        }
+
+    }
+}
